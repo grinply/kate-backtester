8992387f8b9b891772e9ab1513f4246fdf0f0686
Victor Ferreira <victorock20@gmail.com>
2021-04-16 09:29:03 -0300

    formating example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -21,7 +21,7 @@ func main() {
 }
 
 //ProcessNextPriceData process the next data point and checks if a position should be opened
-func (strategy *simpleStrategy) OpenNewPosition(latestPrices []kate.DataPoint) *kate.OpenPositionEvt {
+func (stg *simpleStrategy) OpenNewPosition(latestPrices []kate.DataPoint) *kate.OpenPositionEvt {
 	latest := len(latestPrices) - 1
 
 	if latestPrices[latest].Close > latestPrices[latest-1].Close {
@@ -31,7 +31,7 @@ func (strategy *simpleStrategy) OpenNewPosition(latestPrices []kate.DataPoint) *
 }
 
 //SetStoploss defines a stoploss for the current open position
-func (strategy *simpleStrategy) SetStoploss(openPosition kate.Position) *kate.StoplossEvt {
+func (stg *simpleStrategy) SetStoploss(openPosition kate.Position) *kate.StoplossEvt {
 	if openPosition.Direction == kate.LONG && openPosition.Stoploss <= 0 {
 		return &kate.StoplossEvt{Price: openPosition.EntryPrice * 0.995}
 	}
@@ -39,7 +39,7 @@ func (strategy *simpleStrategy) SetStoploss(openPosition kate.Position) *kate.St
 }
 
 //SetTakeProfit defines a takeprofit for the current open position
-func (strategy *simpleStrategy) SetTakeProfit(openPosition kate.Position) *kate.TakeProfitEvt {
+func (stg *simpleStrategy) SetTakeProfit(openPosition kate.Position) *kate.TakeProfitEvt {
 	if openPosition.Direction == kate.LONG && openPosition.TakeProfit <= 0 {
 		return &kate.TakeProfitEvt{Price: openPosition.EntryPrice * 1.005}
 	}
