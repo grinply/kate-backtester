814a18e5742e07bb72f2878db976ae40feb3ab78
Victor Ferreira <victorock20@gmail.com>
2021-04-22 10:04:33 -0300

    adding comment on price functions

--- kate/prices.go
+++ kate/prices.go
@@ -24,22 +24,27 @@ type OHLCV interface {
 	Volume() float64
 }
 
+//Open is the starting price for a candlestick
 func (candle DataPoint) Open() float64 {
 	return candle.open
 }
 
+//High is the highest price reached between the time a candlestick is open and closed
 func (candle DataPoint) High() float64 {
 	return candle.high
 }
 
+//Low is the lowest price reached between the time a candlestick is open and closed
 func (candle DataPoint) Low() float64 {
 	return candle.low
 }
 
+//Close is the finish price when a candlestick has concluded
 func (candle DataPoint) Close() float64 {
 	return candle.close
 }
 
+//Volume is the amount of assets traded in the timeframe for the current candlestick
 func (candle DataPoint) Volume() float64 {
 	return candle.volume
 }
